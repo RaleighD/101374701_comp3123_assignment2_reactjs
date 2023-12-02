aac5740fb6f1ec975d91a06dfc8b82ae327b4d09
RaleighD <raleigh.b.desmond@gmail.com>
2023-12-02 11:03:50 -0500

    Initialized

---
+++ src/components/signUp.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const Signup = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Here you will handle the user registration logic
+        // This might include validating the input and sending it to a backend server
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+            />
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+            />
+            <button type="submit">Sign Up</button>
+        </form>
+    );
+};
+
+export default Signup;
